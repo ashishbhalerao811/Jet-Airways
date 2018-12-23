96d73afbe6b8e12f73e09d1a346b2db67c28c994
Ashish A. Bhalerao <Ashish A. Bhalerao@DELL>
2018-12-23 15:32:52 +0530

    Class added

---
+++ jetAirways/src/main/java/testBase/JenkinsDemo.java
@@ -0,0 +1,13 @@
+package testBase;
+
+import org.testng.annotations.Test;
+
+public class JenkinsDemo {
+
+	@Test
+	public static void sum() {
+		System.out.println("Welcome to the Jenkind world");
+
+	}
+
+}
